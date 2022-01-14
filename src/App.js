ae08d130b38dad6ccde5e24a589dca25f3417dd2
CASE2  - ilman teguh prasetya <ilmanfrasetya@gmail.com>
2022-01-14 22:45:09 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React,{useEffect,useState} from 'react';
+import axios from 'axios';
+import { DataPersonal } from './DataPersonal';
+axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
 function App() {
+  const [response, setResponse] = useState(null);
+  const [error, setError] = useState('');
+  const [loading, setloading] = useState(true);
+  const fetchData = async () => {
+    await   axios
+          .get('/posts')
+          .then((res) => {
+              setResponse(res.data);
+          })
+          .catch((err) => {
+              setError(err);
+          })
+          .finally(() => {
+              setloading(false);
+          });
+  };
+  useEffect(() => {
+      fetchData();
+  }, []);
+  console.log(response)
+  // let bilangan = [1,2,3,4,5]
+  let tempData = response && response.map((item)=><DataPersonal key={item.userId} name={item.title}/>)
+  // let temp = bilangan.map((item)=><li key={item}>{item}</li>)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Hello World</h3>
+      <ul>
+        {tempData}
+      </ul>
     </div>
   );
 }
---
+++ src/DataPersonal.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const DataPersonal = (props) =>{
+    return(
+        <React.Fragment>
+            <li>{props.name}</li>
+        </React.Fragment>
+    )
+}
